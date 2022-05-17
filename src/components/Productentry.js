833ff6f9d4787a0d968b249b5815363bfa0c4a1d
sruthybabu <sruthybabu64@gmail.com>
2022-05-17 21:45:41 +0530

    Header completed

--- src/components/Productentry.js
+++ src/components/Productentry.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Productentry = () => {
   return (
     <div>
-
+<Header/>
 
 <div className="container">
     <div className="row">
