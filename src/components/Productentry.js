113ed004504497b0c61f8565e115f836178d22ce
sruthybabu <sruthybabu64@gmail.com>
2022-05-17 20:21:59 +0530

    Product entry completed

---
+++ src/components/Productentry.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Productentry = () => {
+  return (
+    <div>
+
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Product Name</label>
+                    <input placeholder="Enter Product Name" type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Price</label>
+                    <input placeholder="Enter Price" type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Description</label>
+                    <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-primary">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Productentry
