7ca5b75e2e865ca74d243b38cc475956422b7443
sruthybabu <sruthybabu64@gmail.com>
2022-05-17 21:05:34 +0530

    Product search completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Productentry from './components/Productentry';
+import Searchproduct from './components/Searchproduct';
 
 function App() {
   return (
     <div>
 
 <Productentry/>
+<Searchproduct/>
 
     </div>
   );
