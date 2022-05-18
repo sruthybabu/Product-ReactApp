6d6c5862bda68836434706051e506be5053a9399
sruthybabu <sruthybabu64@gmail.com>
2022-05-18 19:41:23 +0530

    completed

--- src/components/Productentry.js
+++ src/components/Productentry.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Productentry = () => {
+    var [name,setName]=useState("")
+    var [price,setPrice]=useState("")
+    var [description,setDescription]=useState("")
+
+    const subData=()=>{
+        const data={"name":name,"price":price,"description":description}
+console.log(data)
+    }
+
   return (
     <div>
 <Header/>
@@ -12,18 +21,18 @@ const Productentry = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Product Name</label>
-                    <input placeholder="Enter Product Name" type="text" className="form-control"/>
+                    <input onChange={(e)=>{setName(e.target.value)}} placeholder="Enter Product Name" type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Price</label>
-                    <input placeholder="Enter Price" type="text" className="form-control"/>
+                    <input onChange={(e)=>{setPrice(e.target.value)}} placeholder="Enter Price" type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Description</label>
-                    <textarea placeholder='Enter Description' name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    <textarea onChange={(e)=>{setDescription(e.target.value)}} placeholder='Enter Description' name="" id="" cols="30" rows="10" className="form-control"></textarea>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-primary">SUBMIT</button>
+                    <button onClick={subData} className="btn btn-primary">SUBMIT</button>
                 </div>
             </div>
         </div>
